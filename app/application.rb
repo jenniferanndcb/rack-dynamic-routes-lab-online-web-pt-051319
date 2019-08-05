a8789d93e04da1cd3b9b7aa84b0b4d27744bb413
Jen <jenniferanndcb@gmail.com>
2019-08-05 20:35:30 +0000

    Automatically backed up by Learn

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application 
+  
+  def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new(env)
+    
+    if req.path.match(/items/)
+      item_name = req.path.split("/items/").last 
+      
+      item = @@items.find {|item| item.name == item_name}
+      
+      if item.nil? 
+        resp.write "Item not found"
+        resp.status = 400 
+      else 
+        resp.status = 404
+      end 
+    end 
+    
+    
+  end 
+  
+end 
